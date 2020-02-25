6a753adb70a9bc0d1e6440c93885a2bdcd4fe728
Shubha Rajan <shubhadayini@google.com>
2020-02-25 04:29:20 -0800

    wrote endpoints and integration tests to add and lease databases

---
+++ db-lease-container/db_lease/main.py
@@ -0,0 +1,158 @@
+from quart import Quart, request, jsonify
+from quart.helpers import make_response
+from quart.flask_patch import abort
+from google.cloud import firestore
+import google.cloud.exceptions
+
+
+from datetime import datetime, timedelta
+import re
+import pytz
+from functools import wraps
+import asyncio
+
+app = Quart(__name__)
+
+db = firestore.Client()
+
+DB_TYPES = ["cloud-sql", "cloud-sql-read-replica", "spanner"]
+DB_SIZES = ["1x", "2x", "3x"]
+
+
+def check_required_params(req, keys):
+    """
+    Helper to check if all valid params are present
+    """
+    for key in keys:
+        if key not in req.keys():
+            abort(400)
+
+
+def validate_resource_id(res_id):
+    """
+    Helper to check if resource_id value is valid
+    """
+    return res_id if re.match(r"^[a-z0-9-:]+$", res_id) else abort(400)
+
+
+def validate_db_size(db_size):
+    """
+    Helper to check if database_size value is valid
+    """
+    return db_size if db_size in DB_SIZES else abort(400)
+
+
+def validate_db_type(db_type):
+    """
+    Helper to check if database_type value is valid
+    """
+    return db_type if db_type in DB_TYPES else abort(400)
+
+
+def is_available(resource):
+    """
+    Helper to check if resource is available.
+    """
+    # TODO: change this to inspect "clean" property to find available resource
+    return resource.get("expiry") < datetime.now().replace(tzinfo=pytz.UTC)
+
+
+@firestore.transactional
+def lease(transaction, db_type, size, duration):
+    """
+    Finds the resource with the earliest expiry, and returns it if available.
+    Returns None if no available resource is found.
+    """
+    pool_ref = db.collection(db_type).document(size)
+    query = pool_ref.collection("resources").order_by("expiry").limit(1)
+    resources = query.stream(transaction=transaction)
+    available = None
+    for resource in resources:
+        if is_available(resource):
+            res_ref = pool_ref.collection("resources").document(resource.id)
+            # TODO: set "clean" boolean to false here
+            transaction.update(res_ref, {
+                "expiry": datetime.now() + timedelta(seconds=duration)}
+            )
+            available = resource
+            break
+
+    return available
+
+
+@firestore.transactional
+async def add(transaction, db_type, size, resource_id, ):
+    """
+    Adds a resource with the given id to the pool corresponding to the given
+    database type and size if it doesn't already exist.
+    """
+    pool_ref = db.collection(db_type).document(size)
+    try:
+        pool_ref.collection("resources").document(resource_id).get(
+            transaction=transaction)
+    except google.cloud.exceptions.NotFound:
+        pool_ref = db.collection(db_type).document(size)
+        resource_ref = pool_ref.collection("resources").document(resource_id)
+        transaction.set(resource_ref,
+                        {"expiry": datetime.now() - timedelta(seconds=10)})
+    else:
+        err = make_response(f"Resource {resource_id} already in pool", 400)
+        abort(err)
+
+
+@app.route('/lease', methods=['POST'])
+async def lease_resource():
+    """
+    Route handler which takes database type, database size, and duration as
+    parameters and leases a resource if available.
+    """
+    req_data = await request.get_json()
+    check_required_params(
+        req_data, ["database_type", "database_size", "duration"])
+
+    with db.transaction() as transaction:
+        try:
+            leased_resource = lease(
+                transaction,
+                validate_db_type(req_data["database_type"]),
+                validate_db_size(req_data["database_size"]),
+                req_data["duration"])
+        except Exception as e:
+            err = await make_response(
+                f"An error occurred during the transaction: {e}", 500)
+            return err
+
+    if not leased_resource:
+        err = await make_response("All resources are currently in use", 503)
+        return err
+
+    response = {
+        "resource_id": leased_resource.id,
+        "expiration": leased_resource.get("expiry").timestamp()
+    }
+    return jsonify(response), 200
+
+
+@app.route('/add', methods=['POST'])
+async def add_resource():
+    """
+    Route handler which takes database type, database size, and resource_id as
+    parameters and adds a resource to the appropriate pool.
+    """
+    req_data = await request.get_json()
+    check_required_params(
+        req_data, ["database_type", "database_size", "resource_id"])
+    resource_id = validate_resource_id(req_data["resource_id"])
+    with db.transaction() as transaction:
+        try:
+            add(transaction,
+                validate_db_type(req_data["database_type"]),
+                validate_db_size(req_data["database_size"]),
+                resource_id)
+        except Exception as e:
+            err = await make_response(
+                f"An error occurred during the transaction: {e}", 500)
+            return err
+    return f"Successfully added resource {resource_id} to pool", 200
+
+app.run()
---
+++ db-lease-container/tests/test_db_lease.py
@@ -0,0 +1,91 @@
+import os
+import json
+from unittest import mock
+from datetime import datetime, timedelta
+
+import pytest
+from google.cloud import firestore
+
+from db_lease.main import app
+
+test_app = app.test_client()
+
+
+@pytest.fixture(name="test_db", autouse=True)
+def use_test_db(monkeypatch):
+    test_credentials = os.getenv("TEST_PROJECT_CREDENTIALS")
+    monkeypatch.setenv("GOOGLE_APPLICATION_CREDENTIALS", test_credentials)
+    test_db = firestore.Client()
+    yield test_db
+
+
+@pytest.fixture()
+def resource_available(test_db):
+    pool_ref = test_db.collection(
+        "cloud-sql").document("1x").collection("resources")
+    pool_ref.add({"expiry": datetime.now() - timedelta(seconds=10)})
+    yield
+    for resource in pool_ref.stream():
+        resource.reference.delete()
+
+
+@pytest.fixture()
+def resource_unavailable(test_db):
+    pool_ref = test_db.collection(
+        "cloud-sql").document("1x").collection("resources")
+    pool_ref.add({"expiry": datetime.now() + timedelta(hours=1)})
+    yield
+    for resource in pool_ref.stream():
+        resource.reference.delete()
+
+
+@pytest.mark.asyncio
+async def test_add_resource_to_pool(test_db):
+    client = app.test_client()
+    test_data = {
+        "resource_id": "test-project:us-west2:test-instance",
+        "database_type": "cloud-sql",
+        "database_size": "1x"}
+    headers = {"Content-Type": "application/json"}
+    with mock.patch("db_lease.main.db", test_db):
+        response = await client.post('/add',
+                                     data=json.dumps(test_data),
+                                     headers=headers)
+    resp_data = await response.get_data()
+    assert ("Successfully added resource".encode() in resp_data)
+    assert (response.status_code == 200)
+
+
+@pytest.mark.asyncio
+async def test_lease_resource_when_available(test_db, resource_available):
+    client = app.test_client()
+    test_data = {
+        "database_type": "cloud-sql",
+        "database_size": "1x",
+        "duration": 300}
+    headers = {"Content-Type": "application/json"}
+    with mock.patch("db_lease.main.db", test_db):
+        response = await client.post('/lease',
+                                     data=json.dumps(test_data),
+                                     headers=headers)
+    leased_resource = await response.get_data()
+    assert ("resource_id".encode() in leased_resource)
+    assert ("expiration".encode() in leased_resource)
+    assert (response.status_code == 200)
+
+
+@pytest.mark.asyncio
+async def test_lease_resource_when_unavailable(test_db, resource_unavailable):
+    client = app.test_client()
+    test_data = {
+        "database_type": "cloud-sql",
+        "database_size": "1x",
+        "duration": 300}
+    headers = {"Content-Type": "application/json"}
+    with mock.patch("db_lease.main.db", test_db):
+        response = await client.post('/lease',
+                                     data=json.dumps(test_data),
+                                     headers=headers)
+    resp_data = await response.get_data()
+    assert ("All resources are currently in use".encode() in resp_data)
+    assert (response.status_code == 503)
